676f1168c54c2568ca278d7f607e75a4b827ccb8
otpx5a <otpx5a@gmail.com>
2025-03-10 09:54:01 +0200

    Projekti solution

---
+++ Projektit/Ruudukko koordinaatisto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ruudukko_koordinaatisto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
